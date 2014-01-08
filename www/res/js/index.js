3099f71a4c859eafdcb9b64219e2a26b59021d95
sasserenterprises <daniel@sasserenterprises.com>
2014-01-08 16:22:09 -0600

    0.0.4.5
    added login ajax

--- www/res/js/index.js
+++ www/res/js/index.js
@@ -1,8 +1,27 @@
 // JavaScript Document
-$(document).ready(function(){
-$('#logInB').bind('vclick', function(e){
-	alert('you did it');
+function init() {
+document.addEventListener("deviceready", deviceReady, true);
+delete init;
+}
+
+function deviceReady() {
+
+$("#loginForm").on("submit",function(e) {
+    //disable the button so we can't resubmit while we wait
+    $("#continue",this).attr("disabled","disabled");
+var u = $("#user", this).val();
+var p = $("#password", this).val();
+if(u != '' && p!= '') {
+    $.post("http://www.mrtimeclock.com/scripts/login.cfm", {user:u,password:p}, function(res) {
+        if(res == true) {
+            $.mobile.changePage("some.html");
+        } else {
+            navigator.notification.alert("Your login failed", function() {});
+        }
+         $("#submitButton").removeAttr("disabled");
+    },"json");
+}
+return false;
 });
 
-
-});
+}
